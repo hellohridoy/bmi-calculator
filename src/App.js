601ab1b143b0b06b461051d07abd3210323aa894
Hridoy <ridoy.java@gmail.com>
2023-07-19 11:18:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useMemo, useState } from "react";
 import './App.css';
+const DEFAULT_WEIGHT = 50;
+const DEFAULT_HEIGHT = 150;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[height,setHeight] = useState(DEFAULT_HEIGHT);
+  const[weight,setWeight] = useState(DEFAULT_WEIGHT);
+  
+
+  function onHeightChange(event){
+    const inputHeight = event.target.value;
+    setHeight(inputHeight);
+  }
+
+  function onWeightChange(event){
+    const inputWeight = event.target.value;
+    setWeight(inputWeight);
+  }
+
+  const output = useMemo(() =>{
+    const calculteHeight = height/100;
+    return(weight/(calculteHeight *calculteHeight)).toFixed(1)//toFixed() converts a number to a string, rounded to a specified number of decimals:
+    //toFixed() converts a number to a string, rounded to a specified number of decimals:
+  },[weight,height]);
+
+ return(
+   <div>
+     
+      <h1>Project 7: BMI CALCULATOR</h1>
+      <div className="input-section">
+        <p class="slider-output">Weight: {weight} kg</p>
+        <input
+          className="input-slider"
+          onChange={onWeightChange}
+          type="range"
+          step="1"
+          min="40"
+          max="220"
+        />
+        <p class="slider-output">Height: {height} cm</p>
+        <input
+          className="input-slider"
+          onChange={onHeightChange}
+          type="range"
+          min="140"
+          max="220"
+        />
+      </div>
+      <div className="output-section">
+        <p>Your BMI is</p>
+        <p className="output">{output}</p>
+      </div>
+  
   );
+   </div>
+ )
 }
 
 export default App;
